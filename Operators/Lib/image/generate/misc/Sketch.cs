666ef2d95a3ba4b03441ab4b914a7d122033eba6
1x-vision <1x@1x-vision.net>
2025-05-25 18:33:29 +0200

    improve ui [Sketch]

--- Operators/Lib/image/generate/misc/Sketch.cs
+++ Operators/Lib/image/generate/misc/Sketch.cs
@@ -9,32 +9,32 @@ internal sealed class Sketch : Instance<Sketch>
     [Output(Guid = "6d0b50be-70d4-4539-8d9d-ebb7434075c2")]
     public readonly Slot<BufferWithViews> Points = new();
 
-    [Input(Guid = "1c5d0d86-c000-449e-903a-3212d19d8e1d")]
-    public readonly InputSlot<float> StrokeSize = new();
+        [Input(Guid = "0d965690-5c83-47df-a48f-512e060b5e16")]
+        public readonly InputSlot<T3.Core.DataTypes.Command> Scene = new InputSlot<T3.Core.DataTypes.Command>();
 
-    [Input(Guid = "31f3942e-bac5-407f-ad44-6d09920754d9")]
-    public readonly InputSlot<Vector4> StrokeColor = new();
+        [Input(Guid = "f823fdfd-fc3d-41c4-bd9d-6badf764d702")]
+        public readonly InputSlot<T3.Core.DataTypes.Texture2D> InputImage = new InputSlot<T3.Core.DataTypes.Texture2D>();
 
-    [Input(Guid = "44b88a09-6374-4180-9bc9-713ccfbb36f0")]
-    public readonly InputSlot<Vector4> Background = new();
+        [Input(Guid = "2ded8235-157d-486b-a997-87d09d18f998")]
+        public readonly InputSlot<string> Filename = new InputSlot<string>();
 
-    [Input(Guid = "2ded8235-157d-486b-a997-87d09d18f998")]
-    public readonly InputSlot<string> Filename = new();
+        [Input(Guid = "1c5d0d86-c000-449e-903a-3212d19d8e1d")]
+        public readonly InputSlot<float> StrokeSize = new InputSlot<float>();
 
-    [Input(Guid = "0d965690-5c83-47df-a48f-512e060b5e16")]
-    public readonly InputSlot<Command> Scene = new();
+        [Input(Guid = "31f3942e-bac5-407f-ad44-6d09920754d9")]
+        public readonly InputSlot<System.Numerics.Vector4> StrokeColor = new InputSlot<System.Numerics.Vector4>();
 
-    [Input(Guid = "f823fdfd-fc3d-41c4-bd9d-6badf764d702")]
-    public readonly InputSlot<Texture2D> InputImage = new();
+        [Input(Guid = "44b88a09-6374-4180-9bc9-713ccfbb36f0")]
+        public readonly InputSlot<System.Numerics.Vector4> Background = new InputSlot<System.Numerics.Vector4>();
 
-    [Input(Guid = "d9931f47-2fc9-4df3-ab97-1a71e45501d2")]
-    public readonly InputSlot<bool> ShowOnionSkin = new();
+        [Input(Guid = "d9931f47-2fc9-4df3-ab97-1a71e45501d2")]
+        public readonly InputSlot<bool> ShowOnionSkin = new InputSlot<bool>();
 
-    [Input(Guid = "37093302-053a-47b2-ace6-b9d310d3f4b7")]
-    public readonly InputSlot<int> OverridePageIndex = new();
+        [Input(Guid = "37093302-053a-47b2-ace6-b9d310d3f4b7")]
+        public readonly InputSlot<int> OverridePageIndex = new InputSlot<int>();
 
-    [Input(Guid = "5cdb04d5-9bef-4789-8082-ea04e56b3ca7")]
-    public readonly InputSlot<Int2> Resolution = new();
+        [Input(Guid = "5cdb04d5-9bef-4789-8082-ea04e56b3ca7")]
+        public readonly InputSlot<T3.Core.DataTypes.Vector.Int2> Resolution = new InputSlot<T3.Core.DataTypes.Vector.Int2>();
 
     private enum ShowModes
     {
