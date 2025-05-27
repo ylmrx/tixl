7642a95a882bcf32069991d0a854e6a1ce597b67
mrvux <julien@mrvux.com>
2025-05-27 22:01:31 +0300

    Backup tesselation and geometry shader stage, ensure they are null druing UI drawing, then restore them

--- Editor/Gui/WindowsUiContentDrawer.cs
+++ Editor/Gui/WindowsUiContentDrawer.cs
@@ -289,8 +289,13 @@ public sealed class WindowsUiContentDrawer : IUiContentDrawer<Device>
         var prevPsSampler = _deviceContext.PixelShader.GetSamplers(0, 1);
         var prevPs = _deviceContext.PixelShader.Get();
         var prevVs = _deviceContext.VertexShader.Get();
+        var prevHs = _deviceContext.HullShader.Get();
+        var prevGs = _deviceContext.GeometryShader.Get();
+        var prevDs = _deviceContext.DomainShader.Get();
         var prevVsConstantBuffer = _deviceContext.VertexShader.GetConstantBuffers(0, 1);
         var prevPrimitiveTopology = _deviceContext.InputAssembler.PrimitiveTopology;
+
+
         _deviceContext.InputAssembler.GetIndexBuffer(out var prevIndexBuffer, out var prevIndexBufferFormat, out var prevIndexBufferOffset);
         Buffer[] prevVertexBuffer = new Buffer[1];
         int[] prevVertexBufferOffset = new int[1], prevVertexBufferStride = new int[1];
@@ -311,6 +316,11 @@ public sealed class WindowsUiContentDrawer : IUiContentDrawer<Device>
         _deviceContext.VertexShader.SetConstantBuffer(0, _vertexConstantBuffer);
         _deviceContext.PixelShader.SetShader(_pixelShader, null, 0);
         _deviceContext.PixelShader.SetSampler(0, _fontSampler);
+
+        //make sure we have no tessel/gs
+        _deviceContext.HullShader.Set(null);
+        _deviceContext.DomainShader.Set(null);
+        _deviceContext.GeometryShader.Set(null);
 
         // Setup render state
         _deviceContext.OutputMerger.BlendState = _blendState;
@@ -372,6 +382,9 @@ public sealed class WindowsUiContentDrawer : IUiContentDrawer<Device>
         _deviceContext.PixelShader.SetSamplers(0, prevPsSampler);
         _deviceContext.PixelShader.Set(prevPs);
         _deviceContext.VertexShader.Set(prevVs);
+        _deviceContext.DomainShader.Set(prevDs);
+        _deviceContext.HullShader.Set(prevHs);
+        _deviceContext.GeometryShader.Set(prevGs);
         _deviceContext.VertexShader.SetConstantBuffers(0, prevVsConstantBuffer);
         _deviceContext.InputAssembler.PrimitiveTopology = prevPrimitiveTopology;
         _deviceContext.InputAssembler.SetIndexBuffer(prevIndexBuffer, prevIndexBufferFormat, prevIndexBufferOffset);
