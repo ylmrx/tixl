68be012f33976bb3fd080da9e822c987ec0157ae
Thomas Mann <thomas.mann@framefield.com>
2025-07-06 20:42:59 +0200

    Fix [RepeatAxis] shader code redefiniting of pMod1

--- Operators/Lib/field/space/RepeatAxis.cs
+++ Operators/Lib/field/space/RepeatAxis.cs
@@ -55,17 +55,7 @@ internal sealed class RepeatAxis : Instance<RepeatAxis>
         }
         else
         {
-            c.Globals["pMod1"] = """
-                                 // https://mercury.sexy/hg_sdf/
-                                 // Repeat space along one axis. Use like this to repeat along the x axis:
-                                 // <float cell = pMod1(p.x,5);> - using the return value is optional.
-                                 float pMod1(inout float p, float size) {
-                                     float halfsize = size*0.5;
-                                     float c = floor((p + halfsize)/size);
-                                     p = mod(p + halfsize, size) - halfsize;
-                                     return c;
-                                 }
-                                 """;
+            c.Globals["CommonHgSdf"] = ShaderGraphIncludes.CommonHgSdf;
         }
     }
 
