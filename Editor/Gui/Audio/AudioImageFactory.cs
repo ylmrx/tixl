ee29b97e7dc3e9b61c541f4c029961d4eb808b60
Thomas Mann <thomas.mann@framefield.com>
2025-07-12 22:38:29 +0200

    Fix update of audio background image

--- Editor/Gui/Audio/AudioImageFactory.cs
+++ Editor/Gui/Audio/AudioImageFactory.cs
@@ -19,14 +19,14 @@ internal static class AudioImageFactory
         if (string.IsNullOrEmpty(audioClip.FilePath) || handle.LoadingAttemptFailed)
             return false;
             
-        if (_loadingClips.ContainsKey(audioClip))
+        if (_loadingClips.ContainsKey(audioClip.FilePath))
         {
             imagePath = null;
             return false;
         }
            
         // Return from cache
-        if (_imageForAudioFiles.TryGetValue(audioClip, out imagePath))
+        if (_imageForAudioFiles.TryGetValue(audioClip.FilePath, out imagePath))
         {
             return true;
         }
@@ -38,28 +38,28 @@ internal static class AudioImageFactory
         }
         
             
-        _loadingClips.TryAdd(audioClip, true);
+        _loadingClips.TryAdd(audioClip.FilePath, true);
 
         Task.Run(() =>
                  {
                      Log.Debug($"Creating sound image for {audioClip.FilePath}");
                      if (AudioImageGenerator.TryGenerateSoundSpectrumAndVolume(audioClip, handle.Owner, out var imagePath))
                      {
-                         _imageForAudioFiles[audioClip] = imagePath;
+                         _imageForAudioFiles[audioClip.FilePath] = imagePath;
                      }
                      else
                      {
                          Log.Error($"Failed to create sound image for {audioClip.FilePath}", handle.Owner);
-                         _imageForAudioFiles.TryRemove(audioClip, out _);
+                         _imageForAudioFiles.TryRemove(audioClip.FilePath, out _);
                      }
 
-                     _loadingClips.TryRemove(audioClip, out _);
+                     _loadingClips.TryRemove(audioClip.FilePath, out _);
                  });
             
         return false;
     }
     
     // TODO: should be a hashset, but there is no ConcurrentHashset -_-
-    private static readonly ConcurrentDictionary<AudioClipDefinition, bool> _loadingClips = new();
-    private static readonly ConcurrentDictionary<AudioClipDefinition, string> _imageForAudioFiles = new();
+    private static readonly ConcurrentDictionary<string, bool> _loadingClips = new();
+    private static readonly ConcurrentDictionary<string, string> _imageForAudioFiles = new();
 }
