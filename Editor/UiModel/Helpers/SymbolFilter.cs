3bdbef2ea24bc949d161a0bed9ff32ef5c13acd9
Thomas Mann <thomas.mann@framefield.com>
2025-07-25 20:10:12 +0200

    Some code formatting

--- Editor/UiModel/Helpers/SymbolFilter.cs
+++ Editor/UiModel/Helpers/SymbolFilter.cs
@@ -101,8 +101,6 @@ internal sealed class SymbolFilter
         return true;
     }
 
-
-
     private void UpdateMatchingSymbols(NodeSelection? selection, int limit)
     {
         var compositionInstance = selection?.GetSelectedComposition();
@@ -116,7 +114,7 @@ internal sealed class SymbolFilter
         {
             var symbolUiSymbol = symbolUi.Symbol;
             Debug.Assert(symbolUiSymbol != null);
-            
+
             // Prevent graph cycles
             if (parentSymbolIds.Contains(symbolUiSymbol.Id))
                 continue;
@@ -181,22 +179,18 @@ internal sealed class SymbolFilter
         var symbol = symbolUi.Symbol;
         var symbolName = symbol.Name;
 
-        if(symbol.Namespace.StartsWith("Types.", StringComparison.InvariantCulture))
+        if (symbol.Namespace.StartsWith("Types.", StringComparison.InvariantCulture))
             relevancy *= 4;
-        
+
         if (symbolName.Equals(query, StringComparison.InvariantCultureIgnoreCase))
-        {
             relevancy *= 5;
-        }
 
         if (symbolName.StartsWith(query, StringComparison.InvariantCultureIgnoreCase))
         {
-            // bump if starts
             relevancy *= 4.5f;
         }
         else
         {
-            // bump if direct match
             if (symbolName.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
             {
                 relevancy *= 3f;
@@ -210,9 +204,7 @@ internal sealed class SymbolFilter
         }
 
         if (symbolName.Equals(query, StringComparison.InvariantCultureIgnoreCase))
-        {
             relevancy *= 5;
-        }
 
         // Add usage count (the following statement is slow and should be cached)
         var count = SymbolAnalysis.InformationForSymbolIds.TryGetValue(symbol.Id, out var info)
@@ -224,25 +216,25 @@ internal sealed class SymbolFilter
 
         // Bump if characters match upper characters
         // e.g. "ds" matches "DrawState"
-        var pascalCaseMatch = true;
-        var maxIndex = 0;
-        var uppercaseQuery = query.ToUpper();
-        for (var charIndex = 0; charIndex < uppercaseQuery.Length; charIndex++)
         {
-            var c = uppercaseQuery[charIndex];
-            var indexInName = symbolName.IndexOf(c);
-            if (indexInName < maxIndex)
+            var pascalCaseMatch = true;
+            var maxIndex = 0;
+            var uppercaseQuery = query.ToUpper();
+            for (var charIndex = 0; charIndex < uppercaseQuery.Length; charIndex++)
             {
-                pascalCaseMatch = false;
-                break;
+                var c = uppercaseQuery[charIndex];
+                var indexInName = symbolName.IndexOf(c);
+                if (indexInName < maxIndex)
+                {
+                    pascalCaseMatch = false;
+                    break;
+                }
+
+                maxIndex = indexInName;
             }
 
-            maxIndex = indexInName;
-        }
-
-        if (pascalCaseMatch)
-        {
-            relevancy *= 5f;
+            if (pascalCaseMatch)
+                relevancy *= 5f;
         }
 
         if (!string.IsNullOrEmpty(symbol.Namespace))
@@ -252,25 +244,17 @@ internal sealed class SymbolFilter
                 relevancy *= 0.1f;
 
             if (symbol.Namespace.StartsWith("Lib"))
-            {
                 relevancy *= 3f;
-            }
 
             if (symbol.Namespace.StartsWith("examples"))
-            {
                 relevancy *= 2f;
-            }
         }
 
         if (symbolName.StartsWith("_"))
-        {
             relevancy *= 0.1f;
-        }
 
         if (symbolName.Contains("OBSOLETE"))
-        {
             relevancy *= 0.01f;
-        }
 
         var symbolId = symbol.Id;
         var symbolPackage = symbol.SymbolPackage;
@@ -296,34 +280,15 @@ internal sealed class SymbolFilter
 
             // boost symbols from the same package as composition, or from related namespaces
             if (compositionPackage.Symbols.ContainsKey(symbolId) || symbolPackage.RootNamespace.StartsWith(compositionPackage.RootNamespace))
-            {
                 relevancy *= 1.9f;
-            }
         }
 
         // boost user symbols
         if (symbolPackage is EditableSymbolProject)
-        {
             relevancy *= 1.9f;
-        }
 
         // Bump operators with matching connections 
         var matchingConnectionsCount = 0;
-        // TODO: fix implementation
-        // if (SourceInputHash != 0)
-        // {
-        //     foreach (var inputDefinition in symbol.InputDefinitions.FindAll(i => i.DefaultValue.ValueType == filterInputType))
-        //     {
-        //         var connectionHash = SourceInputHash * 31 + inputDefinition.Id.GetHashCode();
-        //
-        //         if (SymbolAnalysis.ConnectionHashCounts.TryGetValue(connectionHash, out var connectionCount))
-        //         {
-        //             //Log.Debug($" <{connectionCount}x> --> {symbol.Name}");
-        //             matchingConnectionsCount += connectionCount;
-        //         }
-        //     }
-        // }
-
         if (targetInputHash != 0)
         {
             foreach (var outputDefinition in symbol.OutputDefinitions.FindAll(o => o.ValueType == filterOutputType))
@@ -332,7 +297,6 @@ internal sealed class SymbolFilter
 
                 if (SymbolAnalysis.ConnectionHashCounts.TryGetValue(connectionHash, out var connectionCount))
                 {
-                    //Log.Debug($"  {symbol.Name} --> <{connectionCount}x>");
                     matchingConnectionsCount += connectionCount;
                 }
             }
@@ -341,7 +305,6 @@ internal sealed class SymbolFilter
         if (matchingConnectionsCount > 0)
         {
             relevancy *= 1 + MathF.Pow(matchingConnectionsCount, 0.33f) * 4f;
-            //Log.Debug($"Bump relevancy {symbol.Name}  {oldRelevancy:0.00} -> {relevancy:0.00}");
         }
 
         return relevancy;
